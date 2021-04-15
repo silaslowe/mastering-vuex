3079314c27d6ee76d6f2b115193e8227d3a42982
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-04-15 18:44:57 -0500

    lesson 3

--- src/store.js
+++ src/store.js
@@ -4,7 +4,31 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {id: 'abc123', name: 'Adam Jahr'},
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
+    events: [
+      { id: 1, text: 'one', done: true },
+      { id: 2, text: 'two', done: false },
+      { id: 3, text: 'athree', done: true },
+      { id: 4, text: 'four', done: false }
+    ]
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state =>  {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneToDos.length
+      // return state.todos.filter(todo => !todo.done).length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,35 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+<h1>Create an Event, {{user.name}}</h1>
+
+
+<!-- <p>This was created by {{user.id}}</p>
+<p>There are {{catLength}} categories</p>
+<p>{{getEventById(2)}}</p> -->
+<!-- <ul><li v-for="cat in categories" :key="cat">
+  {{cat}}
+  </li></ul> -->
+  </div>
 </template>
+
+<script>
+import {mapState, mapGetters} from 'vuex'
+import DatePicker from 'vuejs-datepicker'
+  export default {
+    components: {
+      DatePicker
+    }
+    // computed: { 
+    // ...mapGetters(['getEventById', 'catLength']),
+    // ...mapState(['user', 'categories']
+    //   //  userName: state => state.user.name,
+    //   //  usrId: state => state.user.id,
+    //   //  categories: state => state.categories
+    // )
+    // }
+  }
+</script>
+
+<style scoped>
+
+</style>
