59feccb052d3ac9e5ec2483dd25efe4772b54f59
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-04-15 20:00:36 -0500

    lesson 4

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
@@ -7,27 +8,22 @@ export default new Vuex.Store({
   state: {
     user: {id: 'abc123', name: 'Adam Jahr'},
     categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
-    events: [
-      { id: 1, text: 'one', done: true },
-      { id: 2, text: 'two', done: false },
-      { id: 3, text: 'athree', done: true },
-      { id: 4, text: 'four', done: false }
-    ]
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({commit}, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
   getters: {
-    catLength: state =>  {
-      return state.categories.length
-    },
-    doneTodos: state => {
-      return state.todos.filter(todo => todo.done)
-    },
-    activeTodosCount: (state, getters) => {
-      return state.todos.length - getters.doneToDos.length
-      // return state.todos.filter(todo => !todo.done).length
-    },
-    getEventById: state => id => {
+    getEventById: state => {
       return state.events.find(event => event.id === id)
     }
   }
